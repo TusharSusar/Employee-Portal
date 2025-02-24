a9d5af72ed3c4e0efe36661bc36960751c02ad01
TusharSusar <tusharsusar06@gmail.com>
2025-02-24 19:10:53 +0530

    Script Updated with border

--- Manager/mangscript.js
+++ Manager/mangscript.js
@@ -7,15 +7,24 @@ const idSearch = document.querySelector('.idSearch');
 const showIDinput = () => {
     idSearch.style.display = 'flex'
     nameSearch.classList.remove('show');
-    serachByID.style.backgroundColor  = '#fff'
-    searchByName.style.backgroundColor  = '#f2f2f2'
+    serachByID.style.backgroundColor  = '#000'
+    searchByName.style.backgroundColor  = '#fff'
+    serachByID.style.color = '#fff'
+    searchByName.style.color = '#fff'
+    searchByName.style.color = '#000'
+    serachByID.style.border = '1px solid #cccccc'
+    // searchByName.style.borderBottom = 'transparent'
 }
 
 const showNameInput = () => {
     idSearch.style.display  = 'none';
     nameSearch.classList.add('show');
-    searchByName.style.backgroundColor  = '#fff'
-    serachByID.style.backgroundColor = '#f2f2f2'
+    searchByName.style.backgroundColor  = '#000'
+    searchByName.style.color = '#fff'
+    serachByID.style.backgroundColor = '#fff'
+    serachByID.style.color = '#000'
+    searchByName.style.border = '1px solid #cccccc'
+    // serachByID.style.borderBottom = 'transparent'
 }
 
 serachByID.addEventListener('click', showIDinput);
